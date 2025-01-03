712e4ad744e434d7278591188d8b807fa350f2e2
darkarnold <arnoldndiwalana@gmail.com>
2025-01-03 12:40:25 +0300

    Add logic to the users service and update user controller to use methods in the users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,96 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Alice Johnson',
+      email: 'alice.johnson@example.com',
+      role: 'admin',
+    },
+    {
+      id: 2,
+      name: 'Bob Smith',
+      email: 'bob.smith@example.com',
+      role: 'intern',
+    },
+    {
+      id: 3,
+      name: 'Charlie Brown',
+      email: 'charlie.brown@example.com',
+      role: 'engineer',
+    },
+    {
+      id: 4,
+      name: 'Diana Lee',
+      email: 'diana.lee@example.com',
+      role: 'admin',
+    },
+    {
+      id: 5,
+      name: 'Ethan Taylor',
+      email: 'ethan.taylor@example.com',
+      role: 'engineer',
+    },
+    {
+      id: 6,
+      name: 'Fiona Davis',
+      email: 'fiona.davis@example.com',
+      role: 'intern',
+    },
+  ];
+
+  findAll(role?: 'Intern' | 'Engineer' | 'Admin') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'Intern' | 'Engineer' | ' Admin';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'Intern' | 'Engineer' | ' Admin';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
